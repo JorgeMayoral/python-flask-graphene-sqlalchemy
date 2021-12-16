71c680e98dec48ab025fe34e77ca4305660f1c1a
Jorge Mayoral Álvarez <jmayoral@corunet.com>
2021-12-16 14:53:39 +0100

    Add filters

--- schema.py
+++ schema.py
@@ -1,7 +1,10 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from graphene_sqlalchemy_filter import FilterableConnectionField, FilterSet
 from models import User as UserModel, Todo as TodoModel
+
+ALL_OPERATIONS = ['eq', 'ne', 'like', 'is_null', 'in', 'not_in', 'lt', 'gt', 'gte', 'range']
 
 
 class User(SQLAlchemyObjectType):
@@ -16,10 +19,31 @@ class Todo(SQLAlchemyObjectType):
         interfaces = (relay.Node,)
 
 
+class UserFilter(FilterSet):
+    class Meta:
+        model = UserModel
+        fields = {
+            'id': ALL_OPERATIONS,
+            'username': ALL_OPERATIONS
+        }
+
+
+class TodoFilter(FilterSet):
+    class Meta:
+        model = TodoModel
+        fields = {
+            'id': ALL_OPERATIONS,
+            'important': ALL_OPERATIONS,
+            'createdAt': ALL_OPERATIONS
+        }
+
+
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     all_todos = SQLAlchemyConnectionField(Todo.connection)
     all_users = SQLAlchemyConnectionField(User.connection, sort=None)
+    user = FilterableConnectionField(connection=User, filters=UserFilter(), sort=User.sort_argument())
+    todo = FilterableConnectionField(connection=Todo, filters=TodoFilter(), sort=Todo.sort_argument())
 
 
 schema = graphene.Schema(query=Query)
